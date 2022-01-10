4effbeefbf989448e8261d6e85ea6a05754dcae3
AlbertoMerkdoP <jacksoon2004@gmail.com>
2022-01-09 23:28:57 -0500

    Setting up server

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const userRoutes = require('./routes/users.route');
 const serviceRoutes = require('./routes/services.route');
+const appointmentRoutes = require('./routes/appointments.route');
 require('dotenv').config()
 require('./db/connection');
 
@@ -9,8 +10,9 @@ const port = process.env.PORT
 const app = express()
 app.use(cors());
 app.use(express.json())
-app.use('/api',userRoutes);
-app.use('/api',serviceRoutes);
+app.use('/api', userRoutes);
+app.use('/api', serviceRoutes);
+app.use('/api', appointmentRoutes);
 
 app.get('/', (req, res) => {
     res.send("Home")
