b414b7bbf6a547163328e8e4e90b8cb034864e0c
AlbertoMerkdoP <jacksoon2004@gmail.com>
2022-01-09 23:27:56 -0500

    Adding token and role verification

--- backend/src/routes/services.route.js
+++ backend/src/routes/services.route.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { getServices, createService, getService, deleteService, updateService, getAvailableServices } = require('../controllers/services.controller');
+const verifyToken = require('../middlewares/verifyToken')
+const verifyRol = require('../middlewares/verifyRol')
 
 //Rutas para los servicios
 
-router.get('/get_services', getServices)
-router.get('/get_service/:id',getService)
-router.get('/get_availableservices', getAvailableServices)
-router.post('/new_service', createService)
-router.put('/update_service/:id', updateService)
-router.delete('/delete_service/:id',deleteService)
+router.get('/get_services', [verifyToken,verifyRol.Planta], getServices)
+router.get('/get_service/:id', [verifyToken,verifyRol.Planta],getService)
+router.get('/get_availableservices', verifyToken, getAvailableServices)
+router.post('/new_service', [verifyToken,verifyRol.Planta], createService)
+router.put('/update_service/:id', [verifyToken,verifyRol.Planta], updateService)
+router.delete('/delete_service/:id', [verifyToken,verifyRol.Planta],deleteService)
 
 module.exports= router;
--- backend/src/routes/users.route.js
+++ backend/src/routes/users.route.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, createUser, getUser, deleteUser, updateUser, loginUser, getMechanics } = require('../controllers/users.controller');
+const { getUsers, createUser, signUp, getUser, deleteUser, updateUser, loginUser, getMechanics } = require('../controllers/users.controller');
+const verifyToken = require('../middlewares/verifyToken')
+const verifyRol = require('../middlewares/verifyRol')
 
 //Rutas para los usuarios
 
-router.get('/get_users', getUsers)
-router.get('/get_user/:id',getUser)
-router.get('/get_mechanics', getMechanics)
-router.post('/new_user', createUser)
+router.get('/get_users', [verifyToken,verifyRol.Administrador],getUsers)
+router.get('/get_user/:id', [verifyToken,verifyRol.Administrador],getUser)
+router.get('/get_mechanics', verifyToken, getMechanics)
+router.post('/new_user', [verifyToken,verifyRol.Administrador], createUser)
 router.post('/login', loginUser)
-router.put('/update_user/:id', updateUser)
-router.delete('/delete_user/:id',deleteUser)
+router.post('/signup', signUp)
+router.put('/update_user/:id', [verifyToken,verifyRol.Administrador], updateUser)
+router.delete('/delete_user/:id', [verifyToken,verifyRol.Administrador],deleteUser)
 
 module.exports= router;
